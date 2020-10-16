3d2c9096bae1ab4e98a5155c5df1443dedb1fef5
libin_ss <libin_ss@kyee.com.cn>
2020-10-16 15:09:00 +0800

    53

---
+++ vue-app/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <base-level :level="1">标题</base-level>
+  </div>
+</template>
+
+<script>
+import BaseLevel from './components/BaseLevel'
+export default {
+  components: {
+    BaseLevel
+  }
+}
+</script>
---
+++ vue-app/src/components/BaseLevel.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  functional: true,
+  props: {
+    level: Number,
+    required: true,
+  },
+  render(h, context) {
+    const { props, slots } = context
+    const tag = 'h' + props.level;
+    return (
+      <tag>
+        { slots().default }
+      </tag>
+    )
+  }
+}
+</script>
